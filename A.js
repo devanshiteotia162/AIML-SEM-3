bee5352355dea5f88d1585ea7790f0bb90417067
devanshiteotia162 <teotia.devanshi2102@gmail.com>
2026-07-31 11:48:08 +0530

    Parameters

---
+++ A.js
@@ -0,0 +1,50 @@
+// console.log("==== Variable ====");
+
+// var text = "Something ";
+// console.log(text);
+// console.log("\n==== If Else ====");
+// var a = 40;
+// var b = 20;
+//  if(a>b) {
+//      console.log("A is greater , value of A=",a);
+//  }
+//  else{
+//      console.log("B is greater , value of B=",b);
+//  }
+// console.log (" \n ==== Function ====");
+// function abes (){
+
+// }
+// console.log ("\n ==== Function with parameter ====");
+// function greet (fname , lname){
+//     console.log ("Hello ", fname , lname );
+//   // missing code 
+// }
+// greet ("Utkarsh ", "Dixit");
+// greet ("Devanshi", "Teotia");
+// console.log (" \n ==== Function Return====");
+// function add (x,y){
+//     return x+y;
+
+// }
+// var result = add (10,20);
+// console.log ("The sum is " +result);
+// console.log ("\n ==== Default Argument ====");
+// function fullName(fname = "ABES", lname = "Engineering College"){
+//     return fname + " " + lname;
+
+// }
+// console.log (fullName());
+// console.log (fullName("Utkarsh", "Dixit"));
+// console.log("\n ==== Percentage ====");
+// function totalMarks( maths , english , science){
+//     return maths + english + science;
+    
+// }
+// function percentage (total){
+//     return (total/300)*100;
+
+// }
+// var total = totalMarks(90, 80, 70);
+// console.log ("Total Marks = " + total);
+// console.log ("Percentage = " + percentage(total) + "%");
---
+++ index.js
@@ -0,0 +1,28 @@
+// var x=20;
+// if(x==20){
+//     console.log("x is equal to 20");
+// }
+
+// var a=20;
+// var b=30;
+// var c=a+b;
+// console.log(c=a+b);
+
+// var x=20;
+// if(x==20){
+//     console.log("x is 20");
+// }
+// else{
+//     console.log("x is not 20");
+// }
+
+// var x=20;
+// const arr=[1,2,3,4,5];
+// for(var i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+//     console.log(arr[2]);
+// }
+
+const app =require('./app.js');
+console.log(app.xz);
+console.log(app.yy);
