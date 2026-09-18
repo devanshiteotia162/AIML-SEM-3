7884057a115cfe83ba79066b710c26db17e0b564
devanshiteotia162 <teotia.devanshi2102@gmail.com>
2026-09-18 12:09:25 +0530

    OOPS

--- oops.js
+++ oops.js
@@ -28,49 +28,49 @@ let s1 = new student (63 , "Devanshi " , 19);
 let s2 = new student (64 , "arjita " , 15);
 
 
-// static property 
-class Hello3 {
-    static name = "Example of static variable and method ";
-    static show (){
-        consoole.log("Hello class this is : " +Hello3.name);
-    }
-}
-Hello3.show();
+// // static property 
+// class Hello3 {
+//     static name = "Example of static variable and method ";
+//     static show (){
+//         consoole.log("Hello class this is : " +Hello3.name);
+//     }
+// }
+// Hello3.show();
 
 
-// static method with static variables 
-class Hello4{
-    static name = "Example of static variable without static variable ";
-    static show (){
-        console.log("Hello class this is : " +Hello4.name)
-    }
-}
-Hello4.show();
+// // static method with static variables 
+// class Hello4{
+//     static name = "Example of static variable without static variable ";
+//     static show (){
+//         console.log("Hello class this is : " +Hello4.name)
+//     }
+// }
+// Hello4.show();
 
-class Hello5 {
-    static name = "Example of static variable and instance variable ";
-    constructor(){
-        this.instanceVar = "Instance Variable ";
+// class Hello5 {
+//     static name = "Example of static variable and instance variable ";
+//     constructor(){
+//         this.instanceVar = "Instance Variable ";
 
-    }
-    show (){
-        console.log("Static variable : " +Hello5.name);
-        console.log("Instance Variable " + this.instanceVar);
+//     }
+//     show (){
+//         console.log("Static variable : " +Hello5.name);
+//         console.log("Instance Variable " + this.instanceVar);
 
-    }
-}
-let h5 = new Hello5();
-h5.show();
-
-
-
-
-// static method with instance method without constructor
+//     }
+// }
+// let h5 = new Hello5();
+// h5.show();
 
 
 
-h6.show();
-class student2{
-    static name = "Devanshi ";
 
-}
+// // static method with instance method without constructor
+
+
+
+// h6.show();
+// class student2{
+//     static name = "Devanshi ";
+
+// }
